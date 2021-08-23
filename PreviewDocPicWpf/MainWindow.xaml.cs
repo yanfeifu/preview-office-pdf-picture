b65852e6bb00b1df5429a6713e904398af67554e
yffu <1425866073@qq.com>
2021-08-23 16:58:58 +0800

    初始化

---
+++ PreviewDocPicWpf/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+using Path = System.IO.Path;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Xps.Packaging;
+
+namespace PreviewDocPicWpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            //Page_Change.Content = new Frame()
+            //{
+            //    Content = pdf
+            //};
+        }
+        
+        public void LoadFile(object sender, RoutedEventArgs e)
+        {
+            //officePage office = new officePage();
+            //Page_Change.Content = new Frame()
+            //{
+            //    Content = office
+            //};
+            pdfPage pdf = new pdfPage();
+            pdf.Loadpdf();
+            Page_Change.Content = new Frame()
+            {
+                Content = pdf
+            };
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //PageChange();
+        //    officePage office = new officePage();
+        //    Page_Change.Content = new Frame()
+        //    {
+        //        Content = office
+        //    };
+        //}
+
+        //private void PageChange()
+        //{
+        //    officePage office = new officePage();
+        //    Page_Change.Content = new Frame()
+        //    {
+        //        Content = office
+        //    };
+        //}
+
+        #region 预览图片
+        //public void PreviewFile(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog openFileDialog = new OpenFileDialog();
+        //    if (openFileDialog.ShowDialog() == true)
+        //    {
+        //        Uri fileUri = new Uri(openFileDialog.FileName);
+        //        this.img.Source = new BitmapImage(fileUri);
+        //    }
+        //}
+        #endregion
+
+        #region 预览pdf
+        //private void PreviewFile(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog dlg = new OpenFileDialog();
+        //    dlg.CheckFileExists = true;
+        //    dlg.Filter = "PowerPoint Format (*.pdf)|*.pdf|" +
+        //                 "All files (*.*)|*.*";
+        //    if ((bool)dlg.ShowDialog(this))
+        //    {
+        //        string filePath = dlg.FileName;
+        //        pdfWebBrowser.Navigate(filePath);
+        //    }
+        //}
+        #endregion
+
+
+    }
+}
+
+
+
---
+++ PreviewDocPicWpf/pdfPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PreviewDocPicWpf
+{
+    /// <summary>
+    /// pdfPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class pdfPage : Page
+    {
+        public pdfPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Loadpdf()
+        {
+            var dialog = new OpenFileDialog();
+            string filePath = dialog.FileName;
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                try
+                {
+                    moonPdfPanel.OpenFile(filePath);
+                    _isLoaded = true;
+                }
+                catch (Exception)
+                {
+                    _isLoaded = false;
+                }
+            }
+        }
+
+        #region 预览pdf
+        private bool _isLoaded = false;
+        private void FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                string filePath = dialog.FileName;
+
+                try
+                {
+                    moonPdfPanel.OpenFile(filePath);
+                    _isLoaded = true;
+                }
+                catch (Exception)
+                {
+                    _isLoaded = false;
+                }
+            }
+        }
+
+        private void ZoomInButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isLoaded)
+            {
+                moonPdfPanel.ZoomIn();
+            }
+        }
+
+        private void ZoomOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isLoaded)
+            {
+                moonPdfPanel.ZoomOut();
+            }
+        }
+
+        private void NormalButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isLoaded)
+            {
+                moonPdfPanel.Zoom(1.0);
+            }
+        }
+
+        private void FitToHeightButton_Click(object sender, RoutedEventArgs e)
+        {
+            moonPdfPanel.ZoomToHeight();
+        }
+
+        private void FacingButton_Click(object sender, RoutedEventArgs e)
+        {
+            moonPdfPanel.ViewType = MoonPdfLib.ViewType.Facing;
+        }
+
+        private void SinglePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            moonPdfPanel.ViewType = MoonPdfLib.ViewType.SinglePage;
+        }
+        #endregion
+    }
+}
